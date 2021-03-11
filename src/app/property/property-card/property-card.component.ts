c3f2752309ffcbb00967c337823e67a4d13f02a1
Nikunj Mandani <nikunj.mandani@gmail.com>
2021-03-11 15:35:25 -0500

    componet generated for nav-bar,property list,property card

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-property-card',
+ // template: ``,
+ templateUrl: 'property-card.component.html',
+ styleUrls : ['property-card.component.css']
+ // styles: ['h1{font-weight: normal;}'],
+})
+export class PropertyCardComponent{
+
+}
