1866565a365ea8ae2c07e2af4faef187e97684f7
Nikunj Mandani <nikunj.mandani@gmail.com>
2021-03-13 13:41:07 -0500

    Using Pipe in HTTP Get method

---
+++ src/app/property/IProperty.interface.ts
@@ -0,0 +1,6 @@
+export interface IProperty {
+  Id:number;
+  Name: string;
+  Type: String;
+  Price : number;
+}
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-list',
@@ -7,83 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  Properties: Array<any> = [
-    {
-      "Id":1,
-      "Name":"Birla House 1",
-      "Type":"flat",
-      "Price": 12000
-    },
-    {
-      "Id":2,
-      "Name":"Birla House 2",
-      "Type":"cottage",
-      "Price": 22000
-    },
-    {
-      "Id":3,
-      "Name":"Birla House 3",
-      "Type":"bunglow",
-      "Price": 32000
-    },
-    {
-      "Id":4,
-      "Name":"Birla House 4",
-      "Type":"darbar",
-      "Price": 42000
-    },
-    {
-      "Id":5,
-      "Name":"Birla House 5",
-      "Type":"cottage",
-      "Price": 52000
-    },
-    {
-      "Id":6,
-      "Name":"Birla House 6",
-      "Type":"bunglow",
-      "Price": 62000
-    },
-    {
-      "Id":7,
-      "Name":"Birla House 7",
-      "Type":"apartment",
-      "Price": 72000
-    },
-    {
-      "Id":8,
-      "Name":"Birla House 8",
-      "Type":"House",
-      "Price": 82000
-    },
-    {
-      "Id":9,
-      "Name":"Birla House 9",
-      "Type":"apartment",
-      "Price": 9000
-    },
-    {
-      "Id":10,
-      "Name":"Birla House 10",
-      "Type":"flat",
-      "Price": 10000
-    },
-    {
-      "Id":11,
-      "Name":"Birla House 11",
-      "Type":"House",
-      "Price": 11000
-    },
-    {
-      "Id":12,
-      "Name":"Birla House 12",
-      "Type":"villa",
-      "Price": 192000
-    }
-  ];
-  constructor() { }
+  Properties: Array<IProperty>;
 
-  ngOnInit() {
+  constructor(private housingService:HousingService) { }
+
+  ngOnInit() :void{
+
+    this.housingService.getAllProperties().subscribe(
+      data=>{
+        this.Properties=data;
+        console.log(data)
+      },error=>{
+        console.log('httperror:');
+        console.log(error);
+      }
+    );
   }
 
 }
