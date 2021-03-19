d073c98ae84ff9807884b598945292fc932effe5
Nikunj Mandani <nikunj.mandani@gmail.com>
2021-03-19 00:46:27 -0400

    alerts added

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/model/user';
 import { UserService } from 'src/app/services/user.service';
+import { AlertifyService } from 'src/app/services/alertify.service';
 
 @Component({
   selector: 'app-user-register',
@@ -13,7 +14,9 @@ export class UserRegisterComponent implements OnInit {
   registerationForm: FormGroup;
   user: User;
   userSubmitted : boolean;
-  constructor(private fb :FormBuilder,private userService:UserService) { }
+  constructor(private fb :FormBuilder,
+              private userService:UserService,
+              private alertify :AlertifyService) { }
 
   ngOnInit() {
     // this.registerationForm = new FormGroup({
@@ -49,6 +52,10 @@ export class UserRegisterComponent implements OnInit {
     this.userService.addUser(this.userData());
     this.registerationForm.reset();
     this.userSubmitted=false;
+    this.alertify.success('Congrats you are successfully registered');
+    } else{
+      this.alertify.error('Kindly provide the required fields');
+
     }
   }
 
